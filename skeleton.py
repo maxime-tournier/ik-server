1281bfac0a20e24e72abd2f866307a9ab303dc37
Maxime Tournier <tournier.maxime@gmail.com>
2015-04-16 11:42:00 +0200

    init commit yay

---
+++ skeleton.py
@@ -0,0 +1,474 @@
+
+
+import numpy as np
+
+from tool import *
+
+skeleton = {
+    
+    'body': {
+
+        'head': {
+            'mass': 5,
+            'dim': [0.3, 0.3, 0.3]
+        },
+
+        'trunk': {
+            'mass': 20,
+            'dim': [0.40, 0.70, 0.30]
+        },
+
+        'femur_left': {
+            'mass': 7,
+            'dim': [0.20, 0.5, 0.30]
+        },
+
+        'femur_right': {
+            'mass': 7,
+            'dim': [0.20, 0.5, 0.30]
+        },
+
+        'tibia_left': {
+            'mass': 5,
+            'dim': [0.20, 0.4, 0.30]
+        },
+
+        'tibia_right': {
+            'mass': 5,
+            'dim': [0.20, 0.4, 0.30]
+        },
+
+        'arm_left': {
+            'mass': 4,
+            'dim': [0.10, 0.4, 0.10]
+        },
+
+        'forearm_left': {
+            'mass': 3,
+            'dim': [0.10, 0.4, 0.10]
+        },
+
+        
+        'arm_right': {
+            'mass': 4,
+            'dim': [0.10, 0.4, 0.10]
+        },
+
+        'forearm_right': {
+            'mass': 3,
+            'dim': [0.10, 0.4, 0.10]
+        },
+
+        'foot_left': {
+            'mass': 2,
+            'dim': [0.10, 0.3, 0.10]
+        },
+
+        'foot_right': {
+            'mass': 2,
+            'dim': [0.10, 0.3, 0.10]
+        },
+
+        
+    },
+    
+
+    'joint': {
+
+        'neck': {
+            "coords": [['head', [0, -0.15, 0]],
+                       ['trunk', [0, 0.35, 0]]],
+            "rest": Quaternion(),
+            "compliance": 1e-1
+        },
+
+        'hip_left': {
+            "coords": [['trunk', [-0.2, -0.35, 0]],
+                       ['femur_left', [0, 0.25, 0]]],
+            "rest": Quaternion(),
+            "compliance": 1e-2
+        },
+
+        'hip_right': {
+            "coords": [['trunk', [0.2, -0.35, 0]],
+                        ['femur_right', [0, 0.25, 0]]],
+            
+            "rest": Quaternion(),
+            "compliance": 1e-2
+        },
+
+        'shoulder_left': {
+             "coords": [['trunk', [-0.25, 0.35, 0]],
+                        ['arm_left', [0, 0.2, 0]]],
+             
+             "rest": Quaternion.exp( -math.pi / 2 * basis(2, 3)),
+             "compliance": 1e-2
+        },
+
+        'shoulder_right': {
+            "coords": [['trunk', [0.25, 0.35, 0]],
+                       ['arm_right', [0, 0.2, 0]]],
+
+            "rest": Quaternion.exp( math.pi / 2 * basis(2, 3)),
+             "compliance": 1e-2
+        },
+
+        'elbow_left': {
+            "coords": [['arm_left', [0, -0.2, 0]],
+                       ['forearm_left', [0, 0.2, 0]]],
+            "rest": Quaternion(),
+            "compliance": 1e-1
+        },
+
+        'elbow_right': {
+            "coords": [['arm_right', [0, -0.2, 0]],
+                       ['forearm_right', [0, 0.2, 0]]],
+            "rest": Quaternion(),
+            "compliance": 1e-1
+        },
+
+        'knee_left': {
+            "coords": [['femur_left', [0, -0.2, 0]],
+                       ['tibia_left', [0, 0.2, 0]]],
+            "rest": Quaternion(),
+            "compliance": 1e-1
+            
+        },
+
+        'knee_right': {
+            "coords": [['femur_right', [0, -0.2, 0]],
+                       ['tibia_right', [0, 0.2, 0]]],
+            "rest": Quaternion(),
+            "compliance": 1e-1
+            
+        },
+
+        'ankle_left': {
+            "coords": [['tibia_left', [0, -0.2, 0]],
+                       ['foot_left', [0, 0.15, 0]]],
+            "rest": Quaternion.exp( -math.pi /2  * basis(0, 3) ),
+            "compliance": 1e-1
+            
+        },
+
+        'ankle_right': {
+            "coords": [['tibia_right', [0, -0.2, 0]],
+                       ['foot_right', [0, 0.15, 0]]],
+            "rest": Quaternion.exp( -math.pi /2  * basis(0, 3) ),
+            "compliance": 1e-1
+            
+        },
+        
+       
+        
+    }
+}
+
+
+
+
+def create( skeleton ):
+
+    class Body: pass
+
+    body = {}
+
+    for i, (k, v) in enumerate( skeleton['body'].iteritems() ):
+        b = Body()
+
+        b.index = i
+        b.name = k
+
+        b.mass = float(v.get('mass', 1))
+        b.dim = np.array( v.get('dim', [1.0, 1.0, 1.0] ) )
+
+        dim2 = b.dim * b.dim
+        b.inertia = b.mass / 12.0 * (sum(dim2) - dim2)
+
+        body[k] = b
+
+
+    class Joint:
+
+        class Coords: pass
+
+    joint = {}
+
+    for i, (k, v) in enumerate( skeleton['joint'].iteritems() ):
+        j = Joint()
+
+        j.index = i
+        j.name = k
+
+        coords = v['coords']
+
+        parent = coords[0]
+        child = coords[1]
+        
+        # TODO info[] ?
+        j.parent = Joint.Coords()
+        j.parent.body = body[ parent[0] ]
+        j.parent.frame = parent[1]
+
+        j.child = Joint.Coords()
+        j.child.body = body[ child[0] ]
+        j.child.frame = child[1]
+        
+        j.rest = v.get('rest', None)
+        j.compliance = v.get('compliance', None)
+
+        # np arrays
+        if j.rest is not None:
+            j.rest = np.array(j.rest).view( Quaternion )
+
+        if j.compliance is not None:
+            j.compliance = np.array(j.compliance)
+            
+        joint[k] = j
+
+    return body, joint
+
+
+
+body, joint = create( skeleton )
+    
+dofs = Rigid3.array( len(body) )
+
+def inertia(body, dofs):
+    dim = 6 * len(body)
+    res = np.zeros( (dim, dim) )
+
+    block = np.zeros( (6, 6) )
+    
+    for b in body.itervalues():
+        i = b.index
+        block[:3, :3] = b.mass * np.identity(3)
+        block[3:, 3:] = np.diag( b.inertia )
+        
+        res[6*i: 6*i + 6,
+            6*i: 6*i + 6] = block
+        
+    return res
+
+
+def attach_constraints(joint, dofs, **kwargs):
+    
+    rows = 3 * len(joint)
+    cols = 6 * len(dofs)
+
+    c = kwargs.get('compliance', 0)
+    
+    matrix = np.zeros( (rows, cols) )
+    value = np.zeros( rows )
+    compliance = np.zeros( rows )
+    
+    for j in joint.itervalues():
+        i = j.index
+        pi = j.parent.body.index
+        ci = j.child.body.index
+
+        Jp = rigid_mapping(dofs[pi], j.parent.frame )
+        Jc = rigid_mapping(dofs[ci], j.child.frame )
+
+        matrix[ 3*i: 3*i + 3, 6 * pi: 6 * pi + 6 ] = Jp
+        matrix[ 3*i: 3*i + 3, 6 * ci: 6 * ci + 6 ] = -Jc
+
+        value[ 3*i: 3*i + 3] = dofs[pi](j.parent.frame) - dofs[ci](j.child.frame)
+
+        compliance[ 3*i: 3*i + 3] = c * np.ones( 3 )
+        
+    return matrix, value, compliance
+
+
+def rest_constraints(joint, dofs, **kwargs):
+
+    num = len( [j for j in joint.itervalues() if j.rest is not None] )
+
+    if num == 0: return None
+    
+    rows = 3 * num
+    cols = 6 * len(dofs)
+    
+    J = np.zeros( (rows, cols) )
+    b = np.zeros( rows )
+    C = np.zeros( rows )
+
+    i = 0
+    for j in joint.itervalues():
+        if j.rest is None: continue
+        
+        pi = j.parent.body.index
+        ci = j.child.body.index
+        
+        Jp, Jc, bi = relative_mapping(dofs[pi], dofs[ci], j.rest)
+
+        J[ 3*i: 3*i + 3, 6 * pi: 6 * pi + 6 ] = Jp
+        J[ 3*i: 3*i + 3, 6 * ci: 6 * ci + 6 ] = Jc
+
+        b[ 3*i: 3*i + 3 ] = bi
+
+        C[ 3*i: 3*i + 3] = j.compliance * np.ones( 3 )
+        i += 1
+        
+    return J, b, C
+
+def constraints(joint, dofs, **kwargs):
+
+    sub = (attach_constraints, rest_constraints)
+    chunks = [c(joint, dofs, **kwargs) for c in sub]
+    
+    J = np.vstack( (c[0] for c in chunks if c ) )
+    b = np.concatenate( tuple(c[1] for c in chunks if c ) )
+    C = np.concatenate( tuple(c[2] for c in chunks if c ) )
+
+    return J, b, C
+        
+
+def rigid_mapping(dofs, local):
+    res = np.zeros( (3, 6) )
+
+    res[:, :3] = np.identity(3)
+
+    R = dofs.orient.matrix()
+    hat = Quaternion.hat(local)
+
+    res[:, 3:] = -R.dot( hat )
+    
+    return res
+
+# TODO: could be group-generic
+def relative_mapping(p, c, rest):
+    Jp = np.zeros( (3, 6) )
+    Jc = np.zeros( (3, 6) )
+
+    relative = p.orient.conj() * c.orient
+    
+    Jp[:, 3:] = -relative.matrix()
+    Jc[:, 3:] = np.identity(3) 
+
+    b = (rest.conj() * relative).flip().log()
+
+    return Jp, Jc, b
+    
+def integrate(dofs, x, dt):
+    for gi, xi in zip(dofs, x):
+        gi.center += dt * xi.linear
+        gi.orient = gi.orient * Quaternion.exp( dt * xi.angular )
+
+
+def step(dt, **kwargs):
+
+    M = inertia(body, dofs)
+
+    L = np.linalg.cholesky(M)
+
+    # TODO better ?
+    Linv = np.linalg.inv(L)
+    
+    while True:
+
+        J, phi, C = constraints(joint, dofs, **kwargs)
+
+        # Minv = Linv.transpose().dot(Linv)
+
+        LinvJT = Linv.dot(J.transpose())
+
+        S = LinvJT.transpose().dot(LinvJT) + np.diag(C / (dt * dt))
+        Sinv = np.linalg.inv(S)
+
+        # TODO external forces
+        f = Rigid3.Deriv.array(len(dofs))
+
+        p = dt * f.flatten()
+        
+        Linvp = Linv.dot(p)
+        b = -phi / dt - LinvJT.transpose().dot(Linvp)
+
+        mu = Sinv.dot(b)
+
+        x = Rigid3.Deriv.view( Linv.transpose().dot(Linvp + LinvJT.dot(mu)) )
+        
+        integrate(dofs, x, dt)
+
+        yield dofs
+
+
+import OpenGL
+from OpenGL.GL import *
+
+
+import pyqglviewer
+class Viewer(pyqglviewer.QGLViewer):
+
+    def animate(self):
+        try:
+            self.dofs = next(self.source)
+        except StopIteration:
+            import sys
+            sys.exit(0)
+
+    def draw(self):
+
+        if 'dofs' not in self.__dict__: return
+        
+        glDisable(GL_LIGHTING)
+
+        glColor(1, 1, 1)
+        glBegin(GL_LINES)
+        
+        for b in self.body.itervalues():
+            g = self.dofs[b.index]
+            c = g.center
+            
+            for i in xrange(3):
+                ei = basis(i, 3)
+                vi = g.orient( ei * b.dim / 2 )
+                
+                glColor(*ei)
+                glVertex(c - vi)
+                glVertex(c + vi)
+            
+        glEnd()
+
+        glColor(1, 1, 1)
+        glPointSize(4)
+        glBegin(GL_POINTS)
+        
+        for j in self.joint.itervalues():
+            p = j.parent.body.index
+            c = j.child.body.index
+            
+            glVertex(self.dofs[p].center + self.dofs[p].orient(j.parent.frame) )
+            glVertex(self.dofs[c].center + self.dofs[c].orient(j.child.frame) )
+            
+        glEnd()
+        
+        glEnable(GL_LIGHTING)
+
+
+    def keyPressEvent(self, e):
+        if e.text() == 'r':
+            for i in self.dofs:
+                i.center = [0, 0, 0]
+                i.orient = Quaternion()
+
+
+
+with pyqglviewer.app():    
+
+    w = Viewer()
+
+    w.body = body
+    w.joint = joint
+
+    w.source = step(1e-1, compliance = 0)
+
+    w.show()
+    w.startAnimation()
+    
+# TODO:
+
+# - JSON
+# - network
+# - external forces
+
---
+++ tool.py
@@ -0,0 +1,249 @@
+import numpy as np
+import math
+import sys
+
+import math
+
+def norm2(x): return x.dot(x)
+def norm(x): return math.sqrt(norm2(x))
+
+def basis(i, n):
+    res = np.zeros(n)
+    res[i] = 1
+    return res
+
+
+class Rigid3(np.ndarray):
+
+    # velocity/forces
+    class Deriv(np.ndarray):
+        def __new__(cls, *args):
+            return np.ndarray.__new__(cls, 6)
+
+        @property
+        def linear(self):
+            return self[:3]
+
+        @linear.setter
+        def linear(self, value):
+            self[:3] = value
+
+
+        @property
+        def angular(self):
+            return self[3:]
+
+        @angular.setter
+        def angular(self, value):
+            self[3:] = value
+
+        @staticmethod
+        def array(size):
+            return np.zeros( (size, 6) ).view(dtype = Rigid3.Deriv )
+
+
+        @staticmethod
+        def view( array ):
+            if len(array) % 6 > 0: raise Exception('bad array size')
+            return array.reshape( (len(array) / 6, 6 ) ).view(dtype = Rigid3.Deriv )
+        
+    @staticmethod
+    def array(size):
+        res = np.zeros( (size, 7) ).view( dtype = Rigid3 )
+        for ri in res:
+            ri.orient.real = 1
+
+        return res
+    
+    @property
+    def center(self):
+        return self[:3]
+
+    @center.setter
+    def center(self, value):
+        self[:3] = value
+
+    @property
+    def orient(self):
+        return self[3:].view(Quaternion)
+
+    @orient.setter
+    def orient(self, value):
+        self[3:] = value
+
+    def __new__(cls, *args):
+        return np.ndarray.__new__(cls, 7)
+        
+    def __init__(self):
+        self[-1] = 1
+        self[:6] = 0
+
+    def inv(self):
+        res = Rigid3()
+        res.orient = self.orient.inv()
+        res.center = -res.orient.rotate(self.center)
+        return res
+
+    def __mul__(self, other):
+        res = Rigid3()
+
+        res.orient = self.orient * other.orient
+        res.center = self.center + self.orient.rotate(other.center)
+        
+        return res
+
+
+    def __call__(self, x):
+        '''applies rigid transform to vector x'''
+        return self.center + self.orient(x)
+    
+
+
+
+    
+class Quaternion(np.ndarray):
+
+    class Deriv(np.ndarray):
+        def __new__(cls, *args):
+            return np.ndarray.__new__(cls, 3)
+
+        @staticmethod
+        def array(size):
+            return np.zeros( (size, 3) ).view(dtype = Deriv)
+
+
+    def __new__(cls, *args):
+        return np.ndarray.__new__(cls, 4)
+        
+    def __init__(self):
+        '''identity quaternion'''
+        self.real = 1
+        self.imag = 0
+        
+    def inv(self):
+        '''inverse'''
+        return self.conj() / self.dot(self)
+    
+    def conj(self):
+        '''conjugate'''
+        res = Quaternion()
+        res.real = self.real
+        res.imag = -self.imag
+
+        return res
+
+    @property
+    def real(self): return self[-1]
+
+    @real.setter
+    def real(self, value): self[-1] = value
+
+    @property
+    def imag(self): return self[:3]
+
+    @imag.setter
+    def imag(self, value): self[:3] = value
+
+    def normalize(self):
+        '''normalize quaternion'''
+        self /= math.sqrt( self.dot(self) )
+
+    def flip(self):
+        '''flip quaternion in the real positive halfplane, if needed'''
+        if self.real < 0: self = -self
+        return self
+    
+    def __mul__(self, other):
+        '''quaternion product'''
+        res = Quaternion()
+        res.real = self.real * other.real - self.imag.dot(other.imag)
+        res.imag = self.real * other.imag + other.real * self.imag + np.cross(self.imag, other.imag)
+        
+        return res
+         
+
+    def __call__(self, x):
+        '''rotate a vector. self should be normalized'''
+        
+        tmp = Quaternion()
+        tmp.real = 0
+        tmp.imag = x
+
+        return (self * tmp * self.conj()).imag
+
+    @staticmethod
+    def hat(x):
+        '''skew-symmetric cross-product matrix'''
+        return np.array( [
+            [0, -x[2], x[1]],
+            [x[2], 0, -x[0]],
+            [-x[1], x[0], 0]
+            ])
+
+    # 1 - 2 q_j^2 - 2 q_k^2 & 2 (q_i q_j + q_k q_r) & 2 (q_i q_k - q_j q_r) \\
+    # 2 (q_i q_j - q_k q_r) & 1 - 2 q_i^2 - 2 q_k^2 & 2 (q_j q_k + q_i q_r) \\
+    # 2 (q_i q_k + q_j q_r) & 2 (q_j q_k - q_i q_r) & 1 - 2 q_i^2 - 2 q_j^2 \\
+
+    def matrix(self):
+        '''quaternion to rotation matrix'''
+        # TODO check !
+        
+        qr = self.real
+        qi = self.imag[0]
+        qj = self.imag[1]
+        qk = self.imag[2]
+
+        qii = qi * qi
+        qjj = qj * qj
+        qkk = qk * qk
+
+        qij = qi * qj
+        qkr = qk * qr
+        qik = qi * qk
+        qjr = qj * qr
+        qjk = qj * qk
+        qir = qi * qr
+        
+        return np.array([
+            [ 1.0 - 2 * (qjj + qkk), 2 * (qij - qkr), 2 * (qik + qjr)],
+            [ 2*(qij + qkr), 1.0 - 2 * (qii + qkk), 2 * (qjk - qir)],
+            [ 2 * (qik - qjr), 2 * (qjk + qir), 1.0 - 2 * (qii + qjj) ]
+            ])
+        
+    
+    @staticmethod
+    def exp(x):
+        '''quaternion exponential (doubled)'''
+
+        theta = norm(x)
+
+        res = Quaternion()
+        
+        if math.fabs(theta) < sys.float_info.epsilon:
+            return res
+
+        s = math.sin(theta / 2.0)
+        c = math.cos(theta / 2.0)
+
+        res.real = c
+        res.imag = x * (s / theta)
+
+        return res
+
+    def log(self):
+        '''(principal) logarithm'''
+
+        sign = 1 if self.real > 0 else -1
+        
+        w = sign * self.real if self.real < 1 else 1
+        
+        half_theta = math.acos( w )
+
+        if math.fabs( half_theta ) < sys.float_info.epsilon:
+            return np.zeros(3)
+
+        # TODO sinc ?
+        
+        # note: we return doubled to be consistent with exp
+        return 2 * (sign * half_theta / math.sin(half_theta) * self.imag)
+
