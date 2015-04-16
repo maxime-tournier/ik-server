fixed jacobian bug + targets

constraint.py
@@ -2,66 +2,76 @@
 import numpy as np
 import mapping
 
+
+# TODO not quite sure attach/rest belong here, should probably be in
+# skeleton
+
 def attach(joint, dofs, **kwargs):
     
-    rows = 3 * len(joint)
+    rows = 3
     cols = 6 * len(dofs)
 
-    c = kwargs.get('compliance', 0)
-    
-    matrix = np.zeros( (rows, cols) )
-    value = np.zeros( rows )
-    compliance = np.zeros( rows )
+    compliance = kwargs.get('compliance', 0)
+
+    res = []
     
     for j in joint.itervalues():
         i = j.index
         pi = j.parent.body.index
         ci = j.child.body.index
 
-        Jp = mapping.rigid(dofs[pi], j.parent.frame )
-        Jc = mapping.rigid(dofs[ci], j.child.frame )
+        Jp, phip = mapping.rigid(dofs[pi], j.parent.frame )
+        Jc, phic = mapping.rigid(dofs[ci], j.child.frame )
 
-        matrix[ 3*i: 3*i + 3, 6 * pi: 6 * pi + 6 ] = Jp
-        matrix[ 3*i: 3*i + 3, 6 * ci: 6 * ci + 6 ] = -Jc
-
-        value[ 3*i: 3*i + 3] = dofs[pi](j.parent.frame) - dofs[ci](j.child.frame)
-
-        compliance[ 3*i: 3*i + 3] = c * np.ones( 3 )
+        J = np.zeros( (rows, cols) )
         
-    return matrix, value, compliance
+        J[ :, 6 * pi: 6 * pi + 6 ] = Jp
+        J[ :, 6 * ci: 6 * ci + 6 ] = -Jc
+
+        phi = phip - phic
+
+        c = compliance * np.ones( 3 )
+
+        res.append( (J, phi, c) )
+
+    return res
 
 
 def rest(joint, dofs, **kwargs):
 
-    num = len( [j for j in joint.itervalues() if j.rest is not None] )
-
-    if num == 0: return None
-    
-    rows = 3 * num
+    rows = 3
     cols = 6 * len(dofs)
     
-    J = np.zeros( (rows, cols) )
-    b = np.zeros( rows )
-    C = np.zeros( rows )
+    res = []
 
-    i = 0
     for j in joint.itervalues():
         if j.rest is None: continue
         
         pi = j.parent.body.index
         ci = j.child.body.index
         
-        Jp, Jc, bi = mapping.relative(dofs[pi], dofs[ci], j.rest)
+        Jp, Jc, phi = mapping.relative(dofs[pi], dofs[ci], j.rest)
 
-        J[ 3*i: 3*i + 3, 6 * pi: 6 * pi + 6 ] = Jp
-        J[ 3*i: 3*i + 3, 6 * ci: 6 * ci + 6 ] = Jc
-
-        b[ 3*i: 3*i + 3 ] = bi
-
-        C[ 3*i: 3*i + 3] = j.compliance * np.ones( 3 )
-        i += 1
+        J = np.zeros( (rows, cols) )
         
-    return J, b, C
+        J[ :, 6 * pi: 6 * pi + 6 ] = Jp
+        J[ :, 6 * ci: 6 * ci + 6 ] = Jc
 
+        c = j.compliance * np.ones( 3 )
+
+        res.append( (J, phi, c) )
+        
+    return res
+
+
+
+
+def merge( chunks ):
+
+    J = np.vstack( (c[0] for c in chunks ) )
+    phi = np.concatenate( tuple(c[1] for c in chunks ) )
+    c = np.concatenate( tuple(c[2] for c in chunks ) )
+
+    return J, phi, c
 
 

mapping.py
@@ -2,6 +2,8 @@ import numpy as np
 from tool import *
 
 def rigid(dofs, local):
+    '''map a point from local to world coordinates in a rigid frame'''
+    
     res = np.zeros( (3, 6) )
 
     res[:, :3] = np.identity(3)
@@ -11,19 +13,20 @@ def rigid(dofs, local):
 
     res[:, 3:] = -R.dot( hat )
     
-    return res
+    return res, dofs(local)
+
 
 # TODO: could be group-generic
 def relative(p, c, rest):
     Jp = np.zeros( (3, 6) )
     Jc = np.zeros( (3, 6) )
 
-    relative = p.orient.conj() * c.orient
+    relative = p.orient.inv() * c.orient
     
-    Jp[:, 3:] = -relative.matrix()
+    Jp[:, 3:] = -relative.inv().matrix()
     Jc[:, 3:] = np.identity(3) 
 
-    b = (rest.conj() * relative).flip().log()
+    b = (rest.inv() * relative).log()
 
     return Jp, Jc, b
     

skeleton.py
@@ -2,6 +2,8 @@
 import numpy as np
 from tool import *
 
+
+
 def create( skeleton ):
 
     class Body: pass
@@ -86,15 +88,11 @@ def inertia(body, dofs):
 import constraint
 
 def constraints(joint, dofs, **kwargs):
-
-    sub = (constraint.attach, constraint.rest)
-    chunks = [c(joint, dofs, **kwargs) for c in sub]
     
-    J = np.vstack( (c[0] for c in chunks if c ) )
-    b = np.concatenate( tuple(c[1] for c in chunks if c ) )
-    C = np.concatenate( tuple(c[2] for c in chunks if c ) )
+    sub = [constraint.attach, constraint.rest
+    ]
+    return sum( (c(joint, dofs, **kwargs) for c in sub), [] )
 
-    return J, b, C
 
 
     

solver.py
@@ -9,26 +9,44 @@ def integrate(dofs, x, dt):
         gi.center += dt * xi.linear
         gi.orient = gi.orient * Quaternion.exp( dt * xi.angular )
 
+import scipy as sp
+from scipy import linalg
 
-def step(dofs, body, joint, **kwargs):
+def step(dofs, inertia, constraints, **kwargs):
 
     dt = kwargs.get('dt', 1)
-    M = skeleton.inertia(body, dofs)
-
+    M = inertia(dofs)
+    
     L = np.linalg.cholesky(M)
-
+    LT = L.transpose()
+    
     # TODO better ?
-    Linv = np.linalg.inv(L)
+    # Linv = np.linalg.inv(L)
+    class Matrix:
+
+        def dot(self, other):
+            return sp.linalg.solve_triangular(L, other, lower = True, check_finite = False)
+
+
+    class MatrixT:
+
+        def dot(self, other):
+            return sp.linalg.solve_triangular(LT, other, lower = False, check_finite = False)
+        
+    Linv = Matrix()
+    LinvT = MatrixT()
     
     while True:
 
-        J, phi, C = skeleton.constraints(joint, dofs, **kwargs)
+        J, phi, c = constraints(dofs)
 
         # Minv = Linv.transpose().dot(Linv)
 
         LinvJT = Linv.dot(J.transpose())
 
-        S = LinvJT.transpose().dot(LinvJT) + np.diag(C / (dt * dt))
+        S = LinvJT.transpose().dot(LinvJT) + np.diag(c / (dt * dt))
+
+        # TODO arg
         Sinv = np.linalg.inv(S)
 
         # TODO external forces
@@ -41,7 +59,7 @@ def step(dofs, body, joint, **kwargs):
 
         mu = Sinv.dot(b)
 
-        x = Rigid3.Deriv.view( Linv.transpose().dot(Linvp + LinvJT.dot(mu)) )
+        x = Rigid3.Deriv.view( LinvT.dot(Linvp + LinvJT.dot(mu)) )
         
         integrate(dofs, x, dt)
 

tool.py
@@ -150,7 +150,7 @@ class Quaternion(np.ndarray):
 
     def flip(self):
         '''flip quaternion in the real positive halfplane, if needed'''
-        if self.real < 0: self = -self
+        if self.real < 0: self[:] = -self
         return self
     
     def __mul__(self, other):
@@ -186,8 +186,7 @@ class Quaternion(np.ndarray):
 
     def matrix(self):
         '''quaternion to rotation matrix'''
-        # TODO check !
-        
+       
         qr = self.real
         qi = self.imag[0]
         qj = self.imag[1]
@@ -233,9 +232,10 @@ class Quaternion(np.ndarray):
     def log(self):
         '''(principal) logarithm'''
 
-        sign = 1 if self.real > 0 else -1
+        sign = 1 if self.real >= 0 else -1
         
-        w = sign * self.real if self.real < 1 else 1
+        w = sign * self.real
+        if w > 1: w = 1
         
         half_theta = math.acos( w )
 
@@ -245,5 +245,5 @@ class Quaternion(np.ndarray):
         # TODO sinc ?
         
         # note: we return doubled to be consistent with exp
-        return 2 * (sign * half_theta / math.sin(half_theta) * self.imag)
+        return 2 * sign * (half_theta / math.sin(half_theta) * self.imag)
 